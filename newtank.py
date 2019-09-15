fded91c5323d72c2bbebbd7274384f56e0485058
KaustubhDamania <kaustubh.damania@gmail.com>
2019-09-15 16:25:19 +0530

    Completed game

--- newtank.py
+++ newtank.py
@@ -1,12 +1,17 @@
 import pygame as py
-import sys,random
-import math
+import sys, random, math
 
 screen = py.display.set_mode((800,800))
 py.init()
 tank_image = py.image.load('realtank.png')
 obstacle_image = py.image.load('rock.png')
 bullet_image = py.image.load('missile.png')
+
+tank_dimensions = tank_image.get_size()
+obstacle_dimensions = obstacle_image.get_size()
+bullet_dimensions = bullet_image.get_size()
+
+game_clock = py.time.Clock()
 
 tank_rect = tank_image.get_rect()
 color = (0,0,0)
@@ -41,6 +46,19 @@ Tank['tank1']['rect'].center = (25,25)
 Tank['tank2']['rect'].center = (750,750)
 obstacles = [(random.randint(40,750),random.randint(40,750)) for i in range(30)]
 
+#checks if the point x,y lies in the circle (x-x1)^2 + (y-y1)^2 = r^2
+def collision(x,y,x1,y1,r):
+    term = (x-x1)**2 + (y-y1)**2
+    return term <= r**2
+
+def obstacle_collision(x,y):
+    radius_obstacle = obstacle_dimensions[0]
+    for i,j in obstacles:
+        i,j = i+radius_obstacle, j+radius_obstacle
+        if collision(x,y,i,j,radius_obstacle):
+            return True
+    return False
+
 def draw():
     screen.fill(color)
     draw_obstacles()
@@ -58,21 +76,47 @@ def rotate_tank(tank, clock):
     tank['rect'] = tank['new_image'].get_rect()
     tank['rect'].center = tank['center']
 
-def translate(tank):
+def translate(tank, direction):
     r = 2
     x,y = tank['center']
-    x += r*math.cos(math.radians(tank['angle']))
-    y -= r*math.sin(math.radians(tank['angle']))
+    x += r*math.cos(math.radians(tank['angle']))*direction
+    y -= r*math.sin(math.radians(tank['angle']))*direction
+    boundary_condition = (0<x<800) and (0<y<800)
+    if obstacle_collision(x,y) or not boundary_condition:
+        return
+    if Tank['tank1'] == tank:
+        center_tank = Tank['tank2']['center']
+    else:
+        center_tank = Tank['tank1']['center']
+
+    width, height = tank_dimensions[0], tank_dimensions[1]
+    condition1 = (center_tank[0]-width/2 < x < center_tank[0]+width/2)
+    condition2 = (center_tank[1]-height/2 < y < center_tank[1]+height/2)
+    if condition1 and condition2:
+        return
     tank['center'] = (x,y)
     tank['rect'].center = (x,y)
 
 def bullet_translate(bullet, tank):
     bullet['rect'].center = tank['center']
     x,y = bullet['rect'].center
-    r = 7
+    r = 10
     while 0 < x < 800 and 0 < y < 800:
         x += r*math.cos(math.radians(tank['angle']))
         y -= r*math.sin(math.radians(tank['angle']))
+        if obstacle_collision(x,y):
+            return
+        if Tank['tank1'] == tank:
+            center_tank = Tank['tank2']['center']
+        else:
+            center_tank = Tank['tank1']['center']
+
+        width, height = tank_dimensions[0], tank_dimensions[1]
+        condition1 = (center_tank[0]-width/2 < x < center_tank[0]+width/2)
+        condition2 = (center_tank[1]-height/2 < y < center_tank[1]+height/2)
+        if condition1 and condition2:
+            print('Game over!')
+            sys.exit()
         bullet['rect'].center = (x,y)
         draw()
         screen.blit(bullet['image'], bullet['rect'])
@@ -87,12 +131,11 @@ while not done:
         if event.type == py.QUIT:
             sys.exit()
             done = True
-
+        
         if event.type == py.KEYDOWN:
             shots_fired1 = event.key == py.K_e
             shots_fired2 = event.key == py.K_RSHIFT
             
-    # print('Shots fired? begin',keys_pressed[py.K_e])
     #Rotation
     if keys_pressed[py.K_a]:
         rotate_tank(Tank['tank1'], 1)
@@ -108,14 +151,19 @@ while not done:
     
     #Translation
     if keys_pressed[py.K_w]:
-        translate(Tank['tank1'])
+        translate(Tank['tank1'],1)
     if keys_pressed[py.K_UP]:
-        translate(Tank['tank2'])
+        translate(Tank['tank2'],1)
+    if keys_pressed[py.K_s]:
+        translate(Tank['tank1'],-1)
+    if keys_pressed[py.K_DOWN]:
+        translate(Tank['tank2'],-1)
     
     #bullet-fire
     if shots_fired1:
         bullet_translate(Tank['tank1']['bullet'], Tank['tank1'])
     if shots_fired2:
         bullet_translate(Tank['tank2']['bullet'], Tank['tank2'])
+    
+    game_clock.tick(30)
 
-    # print('Shots fired? end',keys_pressed[py.K_e])
